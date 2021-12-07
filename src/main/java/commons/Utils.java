aed7fcd046310d1ee104c1fa10840b5d96ffba8e
Matteo Da Rin D'Iseppo <matteo.darindiseppo@prometeia.com>
2021-12-07 23:38:38 +0100

    Day 7 completed!

--- src/main/java/commons/Utils.java
+++ src/main/java/commons/Utils.java
@@ -2,6 +2,7 @@ package commons;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,6 +20,14 @@ public class Utils {
 	
 	public static Scanner getScanner(Class<?> clazz, String pathToFile) throws FileNotFoundException {
 		return new Scanner(new File(clazz.getClassLoader().getResource(pathToFile).getFile()));
+	}
+	
+	public static <T> void print(Iterable<T> iterable) {
+		Iterator<T> iterator = iterable.iterator();
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next().toString() + (iterator.hasNext() ? "," : ""));
+		}
+		System.out.println();
 	}
 
 }
